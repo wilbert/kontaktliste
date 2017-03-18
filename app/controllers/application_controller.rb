147d90d2d2ab415872d53759f7b6fad16c4beb54
Wilbert Ribeiro <wkelyson@gmail.com>
2017-03-18 12:07:58 -0300

    Setup default format to son.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  before_filter :set_default_response_format
+
+  private
+  
+  def set_default_response_format
+    request.format = :json
+  end
 end
