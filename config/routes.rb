94df87a464a9b1d41e673895e8afacc3d665aac3
Wilbert Ribeiro <wkelyson@gmail.com>
2017-03-20 18:30:53 -0300

    Add root path and health check route to help balancer.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :contacts, only: [:index, :show, :update]
+  root 'contacts#index'
+  get '/healthcheck', to: -> _env { [200, { 'Content-Type' => 'text/plain' }, %w(ok)] }
 end
