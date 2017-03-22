60a595b7df7e980dddcd5638df0bce5682370e11
Wilbert Ribeiro <wkelyson@gmail.com>
2017-03-21 22:30:19 -0300

    Translate nationality sort attribute to ransack relationship attribute.

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::ContactsController < ApplicationController
   def index
     params[:q] ||= {}
+
+    params[:q][:s] = 'city_state_acronym' if params[:q][:s] == 'nationality'
+
     @search = Contact.search(params[:q])
     @contacts = @search.result
     @contacts = @contacts.orphans if params[:q][:name_or_email_or_postal_address_cont].blank?
