b9f16c8d8dea6a07b16cd0cc39a59e7d4aa430d0
Wilbert Ribeiro <wkelyson@gmail.com>
2017-03-23 18:21:27 -0300

    Disable cache for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,8 +42,8 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :dalli_store, Rails.application.secrets.memcached_addresses,
-  { namespace: "#{Rails.env}_kontaktliste", expires_in: 1.day, compress: true }
+  # config.cache_store = :dalli_store, Rails.application.secrets.memcached_addresses,
+  # { namespace: "#{Rails.env}_kontaktliste", expires_in: 1.day, compress: true }
 
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
