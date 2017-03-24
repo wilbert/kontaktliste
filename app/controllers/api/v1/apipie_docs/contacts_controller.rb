5e9f90f69900be72fa53daac9f73a64da05e2510
Wilbert Ribeiro <wkelyson@gmail.com>
2017-03-24 10:52:10 -0300

    Remove params q definition.

--- app/controllers/api/v1/apipie_docs/contacts_controller.rb
+++ app/controllers/api/v1/apipie_docs/contacts_controller.rb
@@ -7,7 +7,6 @@ module Api
           api :GET, '/v1/contacts', 'List all available contacts'
           formats ['json']
           description 'Retrieve a list of contacts and association represented in JSON'
-          param :q, Hash, desc: 'Query params for search and sorting using arel', required: false
           api_version '1'
         end
       end
